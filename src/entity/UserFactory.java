6bd75c82c63844b131825fe65b01df765afa68f9
sanya <sanya.chawla@mail.utoronto.ca>
2023-11-05 13:15:31 -0500

    Completed the User entity class and started the UserFactory class

--- src/entity/UserFactory.java
+++ src/entity/UserFactory.java
@@ -1,4 +1,10 @@
 package entity;
 
+import java.util.ArrayList;
+
 public class UserFactory {
+
+    public User create(String name, String email, String password, ArrayList<String> userHistory) {
+        return new User(name, email, password, userHistory);
+    }
 }
