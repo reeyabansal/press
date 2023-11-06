fb5b2a9eab6540af2f948279923240f7ea3f05c0
saketrane <saket.rane2005@gmail.com>
2023-11-06 13:22:13 -0500

    created the Country entity, needs further updating after Article is created

--- src/entity/Country.java
+++ src/entity/Country.java
@@ -1,4 +1,27 @@
 package entity;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Country {
+
+    private String name;
+    private ArrayList<Article> articles;
+
+    public Country(String name){
+        this.name = name;
+        articles = new ArrayList<>();
+    }
+
+    public void Setarticles(){
+        for(int i = 0; i < 5; i++){
+            /*To be un commented when article is made*/
+
+            //articles.add(Article.FindArticles(name));
+        }
+    }
+    public ArrayList<Article> Getarticles(){
+        return this.articles;
+    }
 }
