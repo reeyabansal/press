724db3a42e56e176ea3fa03521196c710281ce28
saketrane <saket.rane2005@gmail.com>
2023-11-13 16:54:35 -0500

    updated Country, created ArticleFactory and NewsAPICaller

---
+++ src/DataAccess/NewsAPICaller.java
@@ -0,0 +1,12 @@
+package DataAccess;
+
+import entity.Article;
+import entity.ArticleFactory;
+
+public class NewsAPICaller implements ArticleFactory {
+
+    @Override
+    public Article createArticle(String parameter, String type) {
+        return null;
+    }
+}
