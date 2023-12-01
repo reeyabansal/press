549ae89fa3d6d09c6bb295d1bd01e3aec0537925
reeyabansal <reeyabansal317@gmail.com>
2023-11-30 20:02:48 -0500

    Working on History Use Case - Completed the DataAccessObject for History of articles use case

--- src/main/java/data_access/HistoryDataAccessObject.java
+++ src/main/java/data_access/HistoryDataAccessObject.java
@@ -4,62 +4,94 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Objects;
 
-public class HistoryDataAccessObject {
-    private final String csv_file = "/assets/data/visited_articles.csv";
-    public HistoryDataAccessObject(){
+import java.io.*;
+import java.util.*;
 
+public class HistoryDataAccessObject {
+    private final String csvPath = "/assets/data/visited_articles.csv";
+    private final File csvFile;
+
+    private final Map<String, Integer> headers = new LinkedHashMap<>();
+
+    private final List<List<String>> visited = new ArrayList<>();
+
+
+    public HistoryDataAccessObject() {
+        csvFile = new File(csvPath);
+
+        headers.put("username", 0);
+        headers.put("title", 1);
+        headers.put("imageUrl", 2);
+        headers.put("description", 3);
+        headers.put("url", 4);
+        headers.put("publishedAt", 5);
+        headers.put("author", 6);
+        headers.put("visited_time", 7);
+
+        if (csvFile.length() == 0) {
+            save();
+        } else {
+            try (BufferedReader reader = new BufferedReader(new FileReader(csvFile))) {
+                String header = reader.readLine();
+
+                // For later: clean this up by creating a new Exception subclass and handling it in the UI.
+//                assert header.equals("username,password,creation_time");
+                String row;
+                while ((row = reader.readLine()) != null) {
+                    String[] col = row.split(",");
+                    visited.add(List.of(col));
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
     }
 
-    public List<List<String>> getHistory(String username) {
-        List<List<String>> articlesHistory = new ArrayList<>();
-        try (BufferedReader reader = new BufferedReader(new FileReader(csv_file))){
-            String header = reader.readLine();
-            String line = reader.readLine();
-            // Loop finds the line where the user
-            while (line != null) {
-                ArrayList<String> lineArticleData = new ArrayList<>(Arrays.asList(line.split(",")));
-                if (Objects.equals(lineArticleData.get(0), username)) {
-                    break;
-                }
-                line = reader.readLine();
+    private void save() {
+        BufferedWriter writer;
+        try {
+            writer = new BufferedWriter(new FileWriter(csvFile));
+            writer.write(String.join(",", headers.keySet()));
+            writer.newLine();
+
+            for (List<String> articleDetails: visited) {
+                String line = String.format("%s,%s,%s,%s,%s,%s,%s,%s",
+                        articleDetails.get(0), articleDetails.get(1), articleDetails.get(2), articleDetails.get(3),
+                        articleDetails.get(4), articleDetails.get(5), articleDetails.get(6), articleDetails.get(7));
+                writer.write(line);
+                writer.newLine();
             }
 
-            // Loops over lines till another user's data is reached
-            assert line != null;
-            ArrayList<String> userArticleData = new ArrayList<>(Arrays.asList(line.split(",")));
-            while (Objects.equals(userArticleData.get(0), username)) {
-                articlesHistory.add(userArticleData.subList(1,userArticleData.size()));
-                line = reader.readLine();
-                if (line == null)
-                    break;
-                userArticleData = new ArrayList<>(Arrays.asList(line.split(",")));
-            }
+            writer.close();
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        return articlesHistory;
     }
 
-    public static void main(String[] args) {
-        HistoryDataAccessObject obj = new HistoryDataAccessObject();
-        List<List<String>> articles = obj.getHistory("reeya");
-
+    public void addToHistory(List<String> article, String username) {
+        List<String> fullDetails = new ArrayList<>();
+        fullDetails.add(username);
+        fullDetails.addAll(article);
+        fullDetails.add(String.valueOf(LocalDateTime.now()));
+        visited.add(fullDetails);
+        this.save();
     }
 
-//    public void updateHistory(String username, List<String> article) {
-//        try (BufferedReader reader = new BufferedReader(new FileReader(csv_file))) {
-//            String header = reader.readLine();
-//            String line = reader.readLine();
-//
-//        }
-//        catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
-
+    public List<List<String>> getHistory(String username) {
+        List<List<String>> visitedArticles = new ArrayList<>();
+        for (List<String> articleDetails : visited) {
+            if (articleDetails.get(0).equals(username)) {
+                visitedArticles.add(articleDetails.subList(1,7));
+            }
+        }
+        return visitedArticles;
+    }
 }
+
