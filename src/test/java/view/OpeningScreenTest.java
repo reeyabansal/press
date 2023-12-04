947bfbc466bf97260d6ba6c4a92650201343eac4
shaeroni <roni.shae@mail.utoronto.ca>
2023-12-04 11:19:01 -0500

    opening screen test start

--- src/test/java/view/OpeningScreenTest.java
+++ src/test/java/view/OpeningScreenTest.java
@@ -1,7 +1,13 @@
 package view;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class OpeningScreenTest {
-
+    @Test
+    void constructor() {
+        OpeningScreen openingScreen = new OpeningScreen();
+        assert(true);
+    }
 }
