ceb2727d4f31534e52651cfafba358825d082bf5
Paarth Arya <63608051+PaarthArya@users.noreply.github.com>
2023-12-05 20:41:58 -0500

    Added refactored Login and Signup use cases

---
+++ src/main/java/interface_adapters/login/LoginPresenter.java
@@ -0,0 +1,42 @@
+package interface_adapters.login;
+
+import interface_adapters.LoggedIn.LoggedInState;
+import interface_adapters.LoggedIn.LoggedInViewModel;
+import interface_adapters.ViewManagerModel;
+import use_case.login.LoginOutputBoundary;
+import use_case.login.LoginOutputData;
+
+public class LoginPresenter implements LoginOutputBoundary {
+
+    private final LoginViewModel loginViewModel;
+    private final LoggedInViewModel loggedInViewModel;
+    private ViewManagerModel viewManagerModel;
+
+    public LoginPresenter(ViewManagerModel viewManagerModel,
+                          LoggedInViewModel loggedInViewModel,
+                          LoginViewModel loginViewModel) {
+        this.viewManagerModel = viewManagerModel;
+        this.loggedInViewModel = loggedInViewModel;
+        this.loginViewModel = loginViewModel;
+    }
+
+    @Override
+    public void prepareSuccessView(LoginOutputData response) {
+        // On success, switch to the logged in view.
+
+        LoggedInState loggedInState = loggedInViewModel.getState();
+        loggedInState.setUsername(response.getUsername());
+        this.loggedInViewModel.setState(loggedInState);
+        this.loggedInViewModel.firePropertyChanged();
+
+        this.viewManagerModel.setActiveView(loggedInViewModel.getViewName());
+        this.viewManagerModel.firePropertyChanged();
+    }
+
+    @Override
+    public void prepareFailView(String error) {
+        LoginState loginState = loginViewModel.getState();
+        loginState.setUsernameError(error);
+        loginViewModel.firePropertyChanged();
+    }
+}
