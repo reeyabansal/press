a0179a4699b025c8b8e5bf76cb949b3c97446ef6
sanya <sanya.chawla@mail.utoronto.ca>
2023-12-06 23:44:19 -0500

    Setup for Search keyword usecase

--- src/main/java/use_case/Search/SearchInputBoundary.java
+++
@@ -1,6 +0,0 @@
-package use_case.Search;
-
-public interface SearchInputBoundary {
-    void execute(SearchInputData searchInputData) throws InterruptedException;
-}
-
--- src/main/java/use_case/Search/SearchOutputBoundary.java
+++
@@ -1,5 +0,0 @@
-package use_case.Search;
-
-public interface SearchOutputBoundary {
-    void prepareView(SearchOutputData searchOutputData);
-}
