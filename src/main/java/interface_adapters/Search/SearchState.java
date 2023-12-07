275da02ea66f6440e0d45a23fa157e538783286d
sanya <sanya.chawla@mail.utoronto.ca>
2023-12-07 00:00:18 -0500

    Implemented the SearchState class

--- src/main/java/interface_adapters/Search/SearchState.java
+++ src/main/java/interface_adapters/Search/SearchState.java
@@ -12,8 +12,14 @@ public class SearchState {
      * @param articles
      */
     public void setArticles(List<List<String>> articles) {
+
         this.articles = articles;
     }
+
+    /**
+     *
+     * @return
+     */
     public List<List<String>> getArticles() {
         // Title, ImageURL, Description, URL, PublishedAt, Author
         return articles;
