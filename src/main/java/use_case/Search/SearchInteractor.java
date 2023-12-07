2b00bbbd24793e7396c2a6178b6b46bf168edb68
sanya <sanya.chawla@mail.utoronto.ca>
2023-12-07 00:07:47 -0500

    Implemented the SearchInteractor class

---
+++ src/main/java/use_case/Search/SearchInteractor.java
@@ -0,0 +1,45 @@
+package use_case.Search;
+
+import entity.Article;
+import entity.ArticleFactory;
+import entity.KeywordArticleFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SearchInteractor implements SearchInputBoundary{
+    final SearchOutputBoundary searchPresenter;
+
+    /**
+     *
+     * @param searchOutputBoundary
+     */
+    public SearchInteractor(SearchOutputBoundary searchOutputBoundary) {
+        this.searchPresenter = searchOutputBoundary;
+    }
+
+    /**
+     *
+     * @param searchInputData
+     * Gets the articles related to the keyword entered by the user. If no articles appear, it moves to Fail view otherwise goes to Success view
+     */
+    public void execute(SearchInputData searchInputData) throws InterruptedException{
+        ArticleFactory articleFactory = new KeywordArticleFactory(searchInputData.getCountryName());
+
+        // n = -1 indicates "get all articles"
+        List<Article> articles = articleFactory.createArticles(-1);
+        List<List<String>> output = new ArrayList<>();
+        for (Article a: articles) {
+            List<String> x = new ArrayList<>();
+            x.add(a.getTitle());
+            x.add(a.getImageUrl());
+            x.add(a.getDescription());
+            x.add(a.getUrl());
+            x.add(a.getPublishedAt());
+            x.add(a.getAuthor());
+            output.add(x);
+        }
+        SearchOutputData searchOutputData = new SearchOutputData(output);
+        searchPresenter.prepareView(searchOutputData);
+    }
+}
